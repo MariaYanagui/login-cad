febc294ada41610cd9c2e96405d156aee0ba2b4b
Maria Yanagui <maria.yanagui@alunoescolassociais.org.br>
2024-08-08 11:29:37 -0300

    colocamos o console.log(dadosLista); como comentario pois foi usado para verificar se o array foi preenchido, logo em seguida arrumamos o campo para digitação da página para ser limpo após uma ação

--- controller.js
+++ controller.js
@@ -18,7 +18,9 @@ function acessar() {
 
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+        // console.log(dadosLista); ( foi apenas usado para ver se o arreio foi preenchido)
+        criaLista();
+        document.getElementById("nomeUser").value = "";
     }else{
         alert("Favor informe um nome para cadastro");
     }
@@ -27,4 +29,10 @@ function acessar() {
 // FUNÇAO PARA CRIAR LISTA DE USUÁRIOS 
 function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Isuário</th></th>Ações</th></tr>";
+    for(let i=0;i <= (dadosListas.length - 1);i++){
+        tabela += "<tr><td>" + dadosLista[i] +  "</td><td></td></tr>";
+        // USADO PARA ADICIONAR UMA LISTA CASO A ANTERIOR FOR USADA
+        document.getElementById("tabela").innerHTML = tabela;
+        // PARA RECEBER O VARIAVEL TABELA COM O ELEMENTO GetElementBYID
+    }
 }
