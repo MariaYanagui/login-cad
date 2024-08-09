f2c67f474c8d7ddd7a268875aea9d4696ffe25de
Maria Yanagui <maria.yanagui@alunoescolassociais.org.br>
2024-08-09 08:22:23 -0300

    adicionando comentários no código do js

--- controller.js
+++ controller.js
@@ -2,13 +2,17 @@
 
 function acessar() {
     let loginEmail = document.getElementById("loginEmail").value;
+    // VALOR DA ENTRADA DO EMAIL 
     let loginSenha = document.getElementById("loginSenha").value;
+    // VALOR DA ENTRADA DA SENHA
 
     if (!loginEmail || !loginSenha) {
         alert("Favor preencher todos os campos!");
+        // VERIFICA SE O CAMPO FOI PREENCHIDO CORRETAMENTE 
     } else {
         // alert("Campos preenchidos com sucesso!");
         window.location.href = "cadastro.html";
+        // USADO PARA LEVAR O USUÁRIO PARA A PÁGINA DE CADASTRO 
     }
 }
 //  FUNÇÃO PARA ARMAZANAMENTO DE NOMES EM ARRY
@@ -21,7 +25,7 @@ function acessar() {
         // console.log(dadosLista); ( foi apenas usado para ver se o arreio foi preenchido)
         criaLista();
         document.getElementById("nomeUser").value = "";
-        // USADO PARA LIMPAR O CAMPO DE DIGITAÇÃO DA PÁGINA APÓS O USO 
+        // USADO PARA LIMPAR O CAMPO DE DIGITAÇÃO DA PÁGINA APÓS O USO (COLOCAR EM BRANCO)
 
     }else{
         alert("Favor informe um nome para cadastro");
@@ -33,7 +37,7 @@ function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     for(let i=0;i <= (dadosLista.length - 1);i++){
         // ACESSA A POSSIÇÃO NA TABELA 
-        tabela += "<tr><td>" + dadosLista[i] +  "</td><td><button type='button'onclick='editar(parentNode.parent.Node.rowIndex)'>Editar</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] +  "</td><td><button type='button'onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button'onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         // USADO PARA ADICIONAR UMA LISTA CASO A ANTERIOR FOR USADA E A CRIAÇÃO DO BOTÃO PARA EDIÇÃO
         document.getElementById("tabela").innerHTML = tabela;
         // PARA RECEBER A LISTA NA VARIAVEL TABELA COM O ELEMENTO GetElementBYID
@@ -45,4 +49,12 @@ function editar(i){
     // TRAZER O VALOR DO ARRAY DE VOLTA PARA O CAMPO
     dadosLista.splice(dadosLista)[(i -1)];
 }
+// FUNÇÃO PARA EXCLUIR NOME DE LISTA
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById("tabela").deleteRow(i);
+    document.getElementById("nomeUser").value = "";
+    // ADICIONEI O UM ELEMENTO PARA DEIXAR O NOME EM BRANCO APÓS O USO DO BOTÃO EXCLUIR 
 
+}
+    
