7d7d6f669b113c10dfb2a703fdafe17d6cf0aecc
Maria Yanagui <maria.yanagui@alunoescolassociais.org.br>
2024-08-30 07:56:45 -0300

    verificação de erro no alert do javascript.

--- controller.js
+++ controller.js
@@ -17,50 +17,57 @@ function acessar() {
     }
 }
 //  FUNÇÃO PARA ARMAZANAMENTO DE NOMES EM ARRY
-    var dadosLista = [];
-    var email = [];
-    function salvarUser(){
-    let nomeUser = document.getElementById ("nomeUser").value;
+var dadosLista = [];
+var email = [];
+
+
+function salvarUser() {
+    let nomeUser = document.getElementById("nomeUser").value;
     let emailUser = document.getElementById("emailUser").value;
 
-    if(nomeUser){
+    // ALERTA CASO O CAMPO ESTEJA VAZIO 
+    if (document.getElementById('emailUser').value == "" || document.getElementById('emailUser').value.indexOf('@') == -1 || document.getElementById('emailUser').value.indexOf('.') == -1) {
+        // Se alguma das condições acima for verdadeira, exibe um alerta
+        alert("Por favor, informe um e-mail válido");
+    } else if (nomeUser && emailUser) {
+        // Adiciona o nome ao array de dadosLista
         dadosLista.push(nomeUser);
-        email.push(emailUser);
-        // console.log(dadosLista); ( virou comentário pois foi apenas usado para ver se o array foi preenchido)
-        criaLista();
+        EmailLista.push(emailUser);
+        // Atualiza a lista exibida na tela
+        criarLista();
+        // Limpa o campo de nome de usuário
         document.getElementById("nomeUser").value = "";
         document.getElementById("emailUser").value = "";
-        // USADO PARA LIMPAR O CAMPO DE DIGITAÇÃO DA PÁGINA APÓS O USO (COLOCAR EM BRANCO)
+    } else {
+        // Se o campo estiver vazio, exibe um alerta
+        alert("Favor informa todos os campos para cadastrado");
+    }
 
-    }else{
-        alert("Favor informe um nome para cadastro");
-    }
-    // ALERTA CASO O CAMPO ESTEJA VAZIO 
-    }
+}
 // FUNÇAO PARA CRIAR LISTA DE USUÁRIOS 
-function criaLista(){
+function criaLista() {
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
-    for(let i=0;i <= (dadosLista.length - 1);i++){
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
         // ACESSA A POSSIÇÃO NA TABELA 
-        tabela += "<tr><td>" + dadosLista[i] +  "</td><td>" + email[i] +  "</td><td><button type='button'onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button'onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + email[i] + "</td><td><button type='button'onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button'onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         // USADO PARA ADICIONAR UMA LISTA CASO A ANTERIOR FOR USADA E A CRIAÇÃO DO BOTÃO PARA EDIÇÃO
         document.getElementById("tabela").innerHTML = tabela;
         // PARA RECEBER A LISTA NA VARIAVEL TABELA COM O ELEMENTO GetElementBYID
     }
 }
 // FUNÇÃO PARA EDITAR NOMES DE LISTA
-function editar(i){
-    document.getElementById("nomeUser").value = dadosLista [(i -1)];
-    document.getElementById("emailUser").value = email [(i -1)];
+function editar(i) {
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+    document.getElementById("emailUser").value = EmailLista[(i - 1)];
     // TRAZER O VALOR DO ARRAY DE VOLTA PARA O CAMPO
     // O método splice faz o quê? altera o conteúdo de uma lista, adicionando novos elementos enquanto remove elementos antigos
     dadosLista.splice(dadosLista[(i - 1)], 1);
     email.splice(email[(i - 1)], 1);
 }
 // FUNÇÃO PARA EXCLUIR NOME DE LISTA
-function excluir(i){
-    dadosLista.splice((i - 1), 1);
-    dadosLista.splice((i - 1), 1);
+function excluir(i) {
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+    emailUser.splice(EmailLista[(i - 1)], 1);
     document.getElementById("tabela").deleteRow(i);
     // O índice passado para deleteRow deve ser verificado, pois a contagem de linhas pode não coincidir com os índices da lista 
     document.getElementById("nomeUser").value = "";
@@ -68,13 +75,3 @@ function excluir(i){
     // ADICIONEI O UM ELEMENTO PARA DEIXAR O NOME EM BRANCO APÓS O USO DO BOTÃO EXCLUIR 
 
 } 
-// VALIDAÇÃO DE EMAIL 
-function checarEmail(){
-    if(document.forms[0].email.value == "" || 
-       document.forms[0].email.value.indexOf("@") == -1 ||
-       document.forms[0].email.value.indexOf(".") == -1)
-       {
-        alert("Por favor, informe um email válido");
-        return false;
-    }
-}
